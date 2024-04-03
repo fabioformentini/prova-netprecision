f5523db23d890103485a573241f8713769ae4d49
Fabio Formentini <fabio.formentini@basis.com.br>
2024-04-03 19:32:12 -0300

    feat: adicionar estrutura do backend

---
+++ lanchonete-service/lanchoneteservice/src/main/java/com/provanetprecision/lanchoneteservice/LanchoneteserviceApplication.java
@@ -0,0 +1,13 @@
+package com.provanetprecision.lanchoneteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchoneteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchoneteserviceApplication.class, args);
+	}
+
+}
---
+++ lanchonete-service/lanchoneteservice/src/test/java/com/provanetprecision/lanchoneteservice/LanchoneteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provanetprecision.lanchoneteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchoneteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3e5f8aa8b46d3b4ada6e3cabcf6b67c6c06e0faa
Fabio Formentini <fabio.formentini@basis.com.br>
2024-04-05 11:37:56 -0300

    feat: remover classe inutilizada

--- lanchonete-service/lanchoneteservice/src/main/java/com/provanetprecision/lanchoneteservice/controller/errors/BadRequestAlertException.java
+++
@@ -1,9 +0,0 @@
-package com.provanetprecision.lanchoneteservice.controller.errors;
-
-import org.zalando.problem.AbstractThrowableProblem;
-import org.zalando.problem.Status;
-
-public class BadRequestAlertException extends AbstractThrowableProblem {
-
-
-}
